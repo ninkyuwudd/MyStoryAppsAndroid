f6bf752983e99820e157d5a88766ed87b148ce29
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2023-10-04 12:24:23 +0700

    token session done cuma ndak beneran login

--- app/src/main/java/com/example/ourstoryapps/login/LoginActivity.kt
+++ app/src/main/java/com/example/ourstoryapps/login/LoginActivity.kt
@@ -1,14 +1,18 @@
 package com.example.ourstoryapps.login
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import com.example.ourstoryapps.R
 import com.example.ourstoryapps.ViewModelFactory
+import com.example.ourstoryapps.data.AkunModel
 import com.example.ourstoryapps.databinding.ActivityLoginBinding
+import com.example.ourstoryapps.homepage.HomepageActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,6 +25,10 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+        viewSetup()
+        actionSetup()
 
 
     }
@@ -40,6 +48,22 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun actionSetup(){
-        binding.
+        binding.lgnButton.setOnClickListener {
+            val email = binding.edLoginEmail.text.toString()
+            viewModel.sessionSave(AkunModel(email,"the_token"))
+            AlertDialog.Builder(this).apply {
+                setTitle("Nice")
+                setMessage("Login Successfully!")
+                setPositiveButton("Next"){
+                    _,_ ->
+                    val itn = Intent(this@LoginActivity,HomepageActivity::class.java)
+                    itn.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(itn)
+                    finish()
+                }
+                create()
+                show()
+            }
+        }
     }
 }
