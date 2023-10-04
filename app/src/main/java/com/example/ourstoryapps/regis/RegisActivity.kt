5c2230eba4e2710c31ff501c137d84c49673ce10
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2023-10-04 12:33:43 +0700

    add register ui

---
+++ app/src/main/java/com/example/ourstoryapps/regis/RegisActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ourstoryapps.regis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ourstoryapps.R
+
+class RegisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regis)
+    }
+}
